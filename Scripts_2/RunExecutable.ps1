48a0c7e5cbf0daa5ffd4b7f710afe0fa91e9aa0e
GeorgeS <shanjin14@users.noreply.github.com>
2020-06-12 11:00:25 +0800

    Add files via upload

---
+++ Scripts_2/RunExecutable.ps1
@@ -0,0 +1,1 @@
+﻿C:\Users\<your user name>\AppData\Local\UiPath\app-20.4.0-beta0718\UiRobot.exe connect --URL "https://cloud.uipath.com/<your_tenant_name>/<your_tenant_folder_name>" --key "<your api_access_key"
